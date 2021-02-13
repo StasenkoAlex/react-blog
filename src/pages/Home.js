c0653dadb1e86686e78f7f30a423baae1cf1b00a
Aleksandra Hrebinnyk <alexandra@MacBook-Air-Aleksandra.local>
2021-02-13 15:36:55 +0200

    init commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Button } from "react-bootstrap";
+
+export default function HomePage() {
+  return (
+    <div>
+      Home page <Button>Button</Button>
+    </div>
+  );
+}
